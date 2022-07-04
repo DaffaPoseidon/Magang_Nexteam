11cfb8e5a2f1e19dcab98c25abf73def7c4080bd
DaffaPoseidon <ndap.cahyo@gmail.com>
2022-07-04 21:50:41 +0700

    Update 2.0

--- minggu-satu/src/App.js
+++ minggu-satu/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Hero from'./components/Hero';
+import Analytics from './components/Analytics';
+import Newsletter from './components/Newsletter';
+import Cards from './components/Cards';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <h1>
       <Navbar/>
+      <Hero />
+      <Analytics/>
+      <Newsletter/>
+      <Cards/>
+      <Footer/>
     </h1>
   );
 }
