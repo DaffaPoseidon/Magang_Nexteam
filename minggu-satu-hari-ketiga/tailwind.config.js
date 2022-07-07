5f2e8f9b5132d4cc122c282f5c91d554b2d087c3
DaffaPoseidon <ndap.cahyo@gmail.com>
2022-07-07 14:18:19 +0700

    Update 2.0

---
+++ minggu-satu-hari-ketiga/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.html", "./src/**/*.js"
+  ],
+  theme: {
+    extend: {
+      'rgb-merah': '236, 0, 140',
+    },
+  },
+  plugins: [],
+}
