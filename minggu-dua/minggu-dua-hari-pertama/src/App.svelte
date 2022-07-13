0b0c072772654bbd68cf5279775baa61fc78f35a
DaffaPoseidon <ndap.cahyo@gmail.com>
2022-07-13 20:21:32 +0700

    User Inputs and Data Binding

--- minggu-dua/minggu-dua-hari-pertama/src/App.svelte
+++ minggu-dua/minggu-dua-hari-pertama/src/App.svelte
@@ -6,9 +6,9 @@
 		characterColor = 'white'
 	}
 
-	const handleInput = (e) => {
-		characterColor = e.target.value
-	}
+	// const handleInput = (e) => {
+	// 	characterColor = e.target.value
+	// }
 </script>
 
 <main>
@@ -46,10 +46,10 @@
 			max-width: none;
 		}
 	}
-	input{
+	/* input{
 		color: black;
 		font-size: 4rem;
 		border: black;
 		padding: 2em 5em;
-	}
+	} */
 </style>
