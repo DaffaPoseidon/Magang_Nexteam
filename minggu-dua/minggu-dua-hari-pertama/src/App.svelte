388ea4ca00742d3f12a93ee3488e7931adbd0791
DaffaPoseidon <ndap.cahyo@gmail.com>
2022-07-13 15:44:02 +0700

    User Input dan Data Binding

--- minggu-dua/minggu-dua-hari-pertama/src/App.svelte
+++ minggu-dua/minggu-dua-hari-pertama/src/App.svelte
@@ -1,19 +1,29 @@
 <script>
 	let name = '2B'
-	let color = 'black'
+	let characterColor = 'black'
 
 	const handleClick = () => {
-		color = 'white'
+		characterColor = 'white'
+	}
+
+	const handleInput = (e) => {
+		characterColor = e.target.value
 	}
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
-	<p>The color is {color}.</p>
+	<p style="color: {characterColor}">The color is {characterColor}.</p>
 
 	<!-- on:click fungsinya seperti IF, kode di bawah berjalan dinamis dan 
 		akan mengubah isi variabel apa pun yang ditentukan dalam function handleClick  -->
 	<button on:click={handleClick}>Update Color</button>
+
+	<!-- One way data binding -->
+	<!-- <input type="text" on:input={handleInput} value={characterColor}> -->
+
+	<!-- Two way data binding -->
+	<input type="text" bind:value={characterColor}/>
 </main>
 
 <style>
@@ -35,5 +45,11 @@
 		main {
 			max-width: none;
 		}
+	}
+	input{
+		color: black;
+		font-size: 4rem;
+		border: black;
+		padding: 2em 5em;
 	}
 </style>
