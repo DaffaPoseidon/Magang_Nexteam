9621d27e1786642ca8a8df7e459df78409f70eab
DaffaPoseidon <ndap.cahyo@gmail.com>
2022-07-14 15:11:46 +0700

    Components

---
+++ minggu-dua/minggu-dua-hari-keempat/Components/src/Modal.svelte
@@ -0,0 +1,17 @@
+<script>
+    // Memberi tahu jika boolean showModal adalah "true" maka akan mencetak HTML div berkelas "backdrop"
+    let showModal = true
+</script>
+
+<!-- Div yang dicetak jika showModal adalah "true" -->
+{#if showModal}
+<div class="backdrop">
+    <div class="modal">
+        <p>Sign up for offers!</p>
+    </div>
+</div>
+{/if}
+
+<style>
+
+</style>
