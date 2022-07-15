b441202fd4c8d2bc5c8d6dd6e89a382fda9d384b
DaffaPoseidon <ndap.cahyo@gmail.com>
2022-07-15 16:57:19 +0700

    Form Satu

---
+++ minggu-dua/minggu-dua-hari-kelima/Form Satu/src/AddPersonForm.svelte
@@ -0,0 +1,22 @@
+<script>
+    let name
+    let characterColor
+    let age
+
+    const handleSubmit = () => {
+        console.log(name, characterColor, age)
+    }
+</script>
+
+<!-- Karena button submit sifatnya adalah memperbarui halaman setiap kali diklik, maka kita menggunakan sesuatu
+     bernama "Event Modifiers preventDefault" sehingga menghapus sifatnya / default-nya -->
+<form on:submit|preventDefault={handleSubmit}>
+    <input type="text" placeholder="name" bind:value={name}>
+    <input type="text" placeholder="character color" bind:value={characterColor}>
+    <input type="number" placeholder="age" bind:value="{age}">
+    <button>Add Person</button>
+</form>
+
+<style>
+
+</style>
