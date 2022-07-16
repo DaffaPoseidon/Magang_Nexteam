bd2d91354191e8aad8849d97e02a93a29af7fc73
DaffaPoseidon <ndap.cahyo@gmail.com>
2022-07-16 15:15:58 +0700

    Header and Footer Components

---
+++ minggu-dua/minggu-dua-hari-keenam/NieR Polls - Header and Footer Components/src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Header from './components/Header.svelte'
+	import Footer from './components/Footer.svelte'
+</script>
+
+<Header/>
+<main>
+	<h1>Hello Androids!</h1>
+	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente expedita sunt distinctio officiis hic quidem amet quas delectus alias voluptatum.</p>
+</main>
+<Footer/>
+
+<style>
+	main{
+		max-width: 960px;
+		margin: 40px auto ;
+	}
+</style>
