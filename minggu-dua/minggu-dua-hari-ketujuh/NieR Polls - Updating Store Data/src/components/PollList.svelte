fa68c80ad6c7c90a6a4b90375a4de24492256c3f
DaffaPoseidon <ndap.cahyo@gmail.com>
2022-07-17 19:15:55 +0700

    NieR Polls - Updating Store Data

---
+++ minggu-dua/minggu-dua-hari-ketujuh/NieR Polls - Updating Store Data/src/components/PollList.svelte
@@ -0,0 +1,38 @@
+<script>
+    // import { onMount, onDestroy } from 'svelte';
+
+    import PollStore from  '../store/PollStore'
+    import PollDetails from './PollDetails.svelte'
+    
+    // export let polls = []
+
+    // const unsub = PollStore.subscribe(data => {
+    //     polls = data
+    // })
+
+    // onMount(() => {
+    //     // Mengambil komponen dari database
+        
+    // })
+
+    // onDestroy(() => {
+    //     // Menghapus komponen
+    //     unsub()
+    // })
+</script>
+
+<div class="poll-list">
+    {#each $PollStore as poll (poll.id)}
+        <div>
+            <PollDetails {poll} />
+        </div>
+    {/each}
+</div>
+
+<style>
+    .poll-list{
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        grid-gap: 20px;
+    }
+</style>
