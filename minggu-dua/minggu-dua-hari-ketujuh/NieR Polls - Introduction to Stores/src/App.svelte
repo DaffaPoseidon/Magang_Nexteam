5cc6a9295458901d19c703253dfa77acdbb54694
DaffaPoseidon <ndap.cahyo@gmail.com>
2022-07-17 17:48:59 +0700

    NieR Polls - Introduction to Stores

---
+++ minggu-dua/minggu-dua-hari-ketujuh/NieR Polls - Introduction to Stores/src/App.svelte
@@ -0,0 +1,55 @@
+<script>
+	import Header from './components/Header.svelte'
+	import Footer from './components/Footer.svelte'
+	import PollList from './components/PollList.svelte'
+	import CreatePollForm from './components/CreatePollForm.svelte'
+	import Tabs from './shared/Tabs.svelte'
+
+	// tabs
+	let items = ['Current Polls', 'Add New Poll']
+	let activeItem = 'Current Polls'
+	const tabChange = (e) => {
+		activeItem = e.detail
+	}
+
+	const handleAdd = (e) => {
+		const poll = e.detail
+		polls = [poll, ...polls]
+		console.log(polls)
+		activeItem = 'Current Polls'
+	}
+
+	const handleVote = (e) => {
+		const {id, option} = e.detail
+		let copiedPolls = [...polls]
+		let upvotedPoll = copiedPolls.find((poll) => poll.id == id)
+
+		if(option === 'a'){
+			upvotedPoll.votesA++
+		}
+		if(option === 'b'){
+			upvotedPoll.votesB++
+		}
+
+		polls = copiedPolls
+	}
+
+</script>
+
+<Header/>
+<main>
+	<Tabs {activeItem} {items} on:tabChange={tabChange}/>
+	{#if activeItem === 'Current Polls'}
+		<PollList on:vote={handleVote}/>
+	{:else if activeItem === 'Add New Poll'}
+		<CreatePollForm on:add={handleAdd}/>
+	{/if}
+</main>
+<Footer/>
+
+<style>
+	main{
+		max-width: 960px;
+		margin: 40px auto;
+	}
+</style>
---
+++ minggu-dua/minggu-dua-hari-ketujuh/NieR Polls - Introduction to Stores/src/components/PollList.svelte
@@ -0,0 +1,25 @@
+<script>
+    import PollStore from  '../store/PollStore'
+    import PollDetails from './PollDetails.svelte'
+    export let polls = []
+
+    PollStore.subscribe(data => {
+        polls = data
+    })
+</script>
+
+<div class="poll-list">
+    {#each polls as poll (poll.id)}
+        <div>
+            <PollDetails {poll} on:vote/>
+        </div>
+    {/each}
+</div>
+
+<style>
+    .poll-list{
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        grid-gap: 20px;
+    }
+</style>
