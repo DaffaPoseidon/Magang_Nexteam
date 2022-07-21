e18cc327c0570d1f05b6b3e047bb94c30693affb
DaffaPoseidon <ndap.cahyo@gmail.com>
2022-07-21 16:46:02 +0700

    SvelteKit - Reset Layouts

---
+++ minggu-tiga/minggu-dua-hari-ketiga/SvelteKit - Reset Layouts/src/routes/guides/__layout@reset.svelte
@@ -0,0 +1,53 @@
+<!-- Fungsi dari class ini adalah memberi tahu SvelteKit bahwa ini 
+     __layout ini yang digunakan dalam folder guides -->
+
+     <script>
+        import Title from '$lib/title.svelte'
+        import '../../styles/global.css'
+    </script>
+    
+    <header>
+        <nav>
+            <Title title="Final Fantasy 8"/>
+            <div class="links">
+                <a href="/">Home</a>
+                <a href="/about">About</a>
+                <a href="/guides">Guides</a>
+            </div>
+        </nav>
+    </header>
+    
+    <main>
+        <!-- konten page -->
+        <slot/>
+    </main>
+    
+    <footer>
+        <p>Final Fantasy Copyright</p>
+    </footer>
+    
+    <style>
+        header{
+            text-align: center;
+            background: rgba(0, 0, 0, 0.1);
+        }
+        main{
+            max-width: 960px;
+            margin: 20px auto;
+        }
+        footer{
+            text-align: center;
+        }
+        .links{
+            margin-left: auto;
+        }
+        a{
+            margin-left: 10px;
+        }
+        nav{
+            display: flex;
+            align-items: center;
+            min-width: 960px;
+            margin: 0 auto;
+        }
+    </style>
