eed4b9f97752e06e76595bb1318ef09af648fef5
DaffaPoseidon <ndap.cahyo@gmail.com>
2022-07-21 17:56:39 +0700

    SvelteKit - Client-side vs Server-side Code

---
+++ minggu-tiga/minggu-tiga-hari-keempat/SvelteKit - Client-side vs Server-side Code/src/lib/title.svelte
@@ -0,0 +1,24 @@
+<!-- Semua yang berada di lib disarankan sebagai Reusable Komponen yang bisa ditempatkan di manapun -->
+
+<script>
+    export let title = "Cloud War"
+</script>
+
+<div class="title">
+    <img src="/meteor_logo.png" alt="Logo FF7">
+    <h1>{title}</h1>
+</div>
+
+<style>
+    .title{
+        display: flex;
+    }
+    .title img{
+        display: inline-block;
+        width: 10vw;
+        height: 10vh;
+    }
+    .title h1{
+        margin-left: 10px;
+    }
+</style>
