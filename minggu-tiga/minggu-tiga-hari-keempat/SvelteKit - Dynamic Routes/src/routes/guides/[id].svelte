4b759924d5984ec1ff9692f4352f45f7424025e8
DaffaPoseidon <ndap.cahyo@gmail.com>
2022-07-21 19:05:44 +0700

    SvelteKit - Dynamic Routes

---
+++ minggu-tiga/minggu-tiga-hari-keempat/SvelteKit - Dynamic Routes/src/routes/guides/[id].svelte
@@ -0,0 +1,46 @@
+<!-- Bagian route yang berubah-ubah / parameter, nama class atau file-nya dibungkus [] -->
+
+<script context="module">
+    export async function load({fetch, page}){
+        const id = page.params.id
+        // karena function-nya tak sinkron maka perlu menunggu hingga sinkron dengan jalannya sistem, lantas
+        // ditambahkan "await"
+        const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+        // variabel guide berisi variabel res yang sudah mengantongi data-data json-nya sehingga bisa digunakan
+        // di manapun
+        const guide = await res.json()
+        console.log(guide)
+
+        // Jika variabel res berjalan dengan baik maka program berlanjut
+        if(res.ok){
+            return {
+                props: {
+                    guide
+                }
+            }
+        }
+
+        // Jika program gagal maka akan mengembalikan status dan informasi error
+        return {
+            status: res.status,
+            error: new Error('Could not fetch the guide')
+        }
+    }
+</script>
+
+<script>
+    export let guide
+</script>
+
+<div class="guide">
+    <h2>{guide.title}</h2>
+    <p>{guide.body}</p>
+</div>
+
+<style>
+    .guide{
+        margin: 40px;
+        padding: 10px;
+        border: 1px dotted rgba(255, 255, 255, 0.2)
+    }
+</style>
